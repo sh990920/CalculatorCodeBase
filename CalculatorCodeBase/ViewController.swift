5b85c7291ed5025b6aaef4994a2e1b77a58a8dc1
sh990920 <sh990920@gmail.com>
2024-06-21 14:23:50 +0900

    Update - Lv1 기능 구현

--- CalculatorCodeBase/ViewController.swift
+++ CalculatorCodeBase/ViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    var textLable = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .black
+        textLableSetting()
     }
 
+    private func textLableSetting() {
+        textLable.text = "12345"
+        textLable.textColor = .white
+        textLable.font = UIFont.boldSystemFont(ofSize: 60)
+        textLable.textAlignment = .right
+        view.addSubview(textLable)
+        
+        textLable.snp.makeConstraints{
+            $0.height.equalTo(100)
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.top.equalToSuperview().offset(200)
+        }
+    }
 
 }
 
