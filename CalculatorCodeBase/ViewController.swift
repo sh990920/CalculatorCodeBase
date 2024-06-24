4207bbfb7d052198b4e534b5a0003772f39e14e9
sh990920 <sh990920@gmail.com>
2024-06-24 15:00:08 +0900

    Update - Lv8 기능 구현

--- CalculatorCodeBase/ViewController.swift
+++ CalculatorCodeBase/ViewController.swift
@@ -10,27 +10,25 @@ import SnapKit
 
 class ViewController: UIViewController {
     
-    var textLable = UILabel()
+    var textLabel = UILabel()
+    var verticalStackView = UIStackView()
     let numbers = [["7", "8", "9", "+"], ["4", "5", "6", "-"], ["1", "2", "3", "x"], ["AC", "0", "=", "÷"]]
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .black
-        textLableSetting()
-        let stackView = createVerticalStackView()
-        
+        textLabelSetting()
+        createVerticalStackView()
     }
 
-    private func textLableSetting() {
-        textLable.text = "12345"
-        textLable.textColor = .white
-        textLable.font = UIFont.boldSystemFont(ofSize: 60)
-        textLable.textAlignment = .right
-        view.addSubview(textLable)
+    private func textLabelSetting() {
+        textLabel.text = "0"
+        textLabel.textColor = .white
+        textLabel.font = UIFont.boldSystemFont(ofSize: 60)
+        textLabel.textAlignment = .right
+        view.addSubview(textLabel)
         
-        textLable.snp.makeConstraints{
+        textLabel.snp.makeConstraints{
             $0.height.equalTo(100)
             $0.leading.equalToSuperview().offset(30)
             $0.trailing.equalToSuperview().offset(-30)
@@ -38,44 +36,37 @@ class ViewController: UIViewController {
         }
     }
     
-    private func createVerticalStackView() -> UIStackView {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.backgroundColor = .black
-        stackView.spacing = 10
-        stackView.distribution = .fillEqually
-        view.addSubview(stackView)
-        stackView.snp.makeConstraints { make in
+    private func createVerticalStackView() {
+        verticalStackView.axis = .vertical
+        verticalStackView.backgroundColor = .black
+        verticalStackView.spacing = 10
+        verticalStackView.distribution = .fillEqually
+        view.addSubview(verticalStackView)
+        verticalStackView.snp.makeConstraints { make in
             make.width.equalTo(350)
             make.centerX.equalToSuperview()
-            make.top.equalTo(textLable.snp.bottom).offset(60)
+            make.top.equalTo(textLabel.snp.bottom).offset(60)
         }
-        var horizontals = Array<UIStackView>()
         for i in numbers {
-            let horizontal = createHorizontalStackView(stackView, i)
-            horizontals.append(horizontal)
+            let horizontal = createHorizontalStackView(i)
+            verticalStackView.addArrangedSubview(horizontal)
+            horizontal.snp.makeConstraints {
+                $0.height.equalTo(80)
+                $0.width.equalTo(350)
+                $0.centerX.equalToSuperview()
+            }
         }
-        
-        horizontals.forEach{ stackView.addArrangedSubview($0) }
-        
-        return stackView
     }
     
-    private func createHorizontalStackView(_ uiView : UIView, _ numbers: [String]) -> UIStackView {
+    private func createHorizontalStackView(_ numbers: [String]) -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.backgroundColor = .black
         stackView.spacing = 10
         stackView.distribution = .fillEqually
-        uiView.addSubview(stackView)
         for i in numbers {
             let btn = createButton(i)
             stackView.addArrangedSubview(btn)
-        }
-        stackView.snp.makeConstraints {
-            $0.height.equalTo(80)
-            $0.width.equalTo(350)
-            $0.centerX.equalToSuperview()
         }
         return stackView
     }
@@ -85,7 +76,7 @@ class ViewController: UIViewController {
         let button = UIButton()
         button.setTitle(text, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 30)
-        if let num = Int(text) {
+        if Int(text) != nil {
             button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
             button.addTarget(self, action: #selector(didTapNumberButton), for: .touchUpInside)
         } else {
@@ -99,11 +90,58 @@ class ViewController: UIViewController {
     }
     
     @objc func didTapNumberButton(_ sender: UIButton) {
-        print("숫자 버튼 누름")
+        if let str = textLabel.text {
+            if let text = sender.titleLabel?.text {
+                if let num = Int(str), num == 0 {
+                    textLabel.text = text
+                } else {
+                    textLabel.text = str + text
+                }
+            }
+        }
     }
     
     @objc func didTapPlusButton(_ sender: UIButton) {
-        print("더하기 버튼 누름")
+        if let text = sender.titleLabel?.text {
+            switch text {
+            case "AC":
+                textLabel.text = "0"
+            case "=":
+                if let expression = textLabel.text {
+                    if let result = calculate(expression: expression) {
+                        textLabel.text = String(result)
+                    }
+                }
+            default:
+                if let textLabelText = textLabel.text {
+                    textLabel.text = textLabelText + text
+                }
+            }
+        }
     }
-
+    
+    func isValidExpression(_ expression: String) -> Bool {
+        let trimmedExpression = expression.replacingOccurrences(of: " ", with: "")
+        let pattern = "^[0-9]+([+\\-*/][0-9]+)*$"
+        if let _ = trimmedExpression.range(of: pattern, options: .regularExpression) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func calculate(expression: String) -> Int? {
+        let str = expression.replacingOccurrences(of: "x", with: "*").replacingOccurrences(of: "÷", with: "/")
+        guard isValidExpression(str) else {
+            print("Error : 수식이 올바르지 않습니다.")
+            return nil
+        }
+        let express = NSExpression(format: str)
+        if let result = express.expressionValue(with: nil, context: nil) as? NSNumber {
+            return result.intValue
+        } else {
+            print("Error : 수식이 잘못됨")
+            return nil
+        }
+    }
 }
